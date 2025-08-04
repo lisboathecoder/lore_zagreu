457b3fd99e871e7c48015cf2a442bd95be75f253
lisboathecoder <gustavo.lisboa2009@gmail.com>
2025-08-04 11:54:24 -0300

    feat: Capítulo 1

--- lore.js
+++ lore.js
@@ -39,16 +39,37 @@ console.log(`Sua arma principal é uma lâmina, mais conhecida como ${NOMEARMA}.
 console.log(`Nyx a Mãe Noite. é a conselheira de ${NOMEPERSONAGEM}, e ela quer dizer algo para ${NOMEPERSONAGEM}`);
 if (podeSair) {
     console.log(`🌑 Nyx: Você está preparado para sua jornada meu querido ${NOMEPERSONAGEM}, te presentei-o com ${NOMEARMADURA}`);
-    defesaBase += 100;
+    defesaTotal += 100;
 } else {
     console.log(`🌑 Nyx: Treine mais querido, desse jeito você não irá vencer seu pai`);
-    console.log(` `);
+    console.log(`===========================================================`);
     console.log(`🩻 Treino de ${NOMEPERSONAGEM}`);
     console.log(`Skelly já está pronto para ser espancado, ele é o boneco de treino de ${NOMEPERSONAGEM}`);
-    console.log(` `);
+    console.log(`===========================================================`);
     console.log(`Após um longo treino, ${NOMEPERSONAGEM} está pronto e Nyx o chama novamente.`);
     console.log(`🌑 Nyx: Você está preparado para sua jornada meu querido ${NOMEPERSONAGEM}, te presentei-o com ${NOMEARMADURA}`);
-    defesaBase += 100;
+    defesaTotal += 100;
 }
 console.log(`Suas defesas é um dos seus pontos fortes, ainda mais com uma ajuda de Nyx, com isso seus pontos de defesa são ${defesaTotal}.`);
 console.log(`${NOMEPERSONAGEM} está pronto para partir e enfrentar seu pai em busca da liberdade do Submundo.`);
+console.log(`===========================================================`);
+console.log("🔴 Capítulo 1 - A primeira tentativa");
+console.log(`===========================================================`);
+localAtual = "Tártaro";
+console.log(`${NOMEPERSONAGEM} entra furtivamente no ${localAtual} e surge Poseidon que surpreende ${NOMEPERSONAGEM}`);
+if (ataqueForte || vidaSuficiente) {
+    ataqueTotal += 100;
+    vida += 50
+    console.log(`🔱 Poseindon: Meu sobrinho ${NOMEPERSONAGEM}, concedo para ti o poder dos MARES! (Ataque e Vida Atuais: ${ataqueTotal} pontos de ataque e ${vida} pontos de vida)`);
+} else {
+    console.log(`🔱 Poseindon: ${NOMEPERSONAGEM}, você não merece nenhum dos meus poderes! Mas pode continuar com a sua jornada.`);
+}
+console.log(`${NOMEPERSONAGEM} agora etá com ${vida} pontos de vida, ${defesaTotal} pontos de defesa e ${ataqueTotal} pontos de ataque!`);
+console.log(`===========================================================`);
+console.log(`${NOMEPERSONAGEM} depois de aniquilar com algumas almas mais fracas, tropeça de frente com os gemêos Bombardeiros do Inferno, e tem que fazer uma escolha!`);
+let podeEnfrentarBombardeiros = ataqueForte && vidaSuficiente && defesaTotal >= 70;
+if (podeEnfrentarBombardeiros) {
+    console.log(`${NOMEPERSONAGEM} não veio até aqui a toa, ele irá enfrentar esses inimigos que estão no seu caminho!`);
+} else {
+    console.log(`${NOMEPERSONAGEM} não está preparado para essa luta, ele voltará para a Casa de Hades!`);
+}
